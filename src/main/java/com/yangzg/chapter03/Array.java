1b62d6a4dad1e9ef71f69d61193335ac9371ca3b
Sam <sam@micous.com>
2019-06-10 21:32:31 +0800

    array

---
+++ src/main/java/com/yangzg/chapter03/Array.java
@@ -0,0 +1,32 @@
+package com.yangzg.chapter03;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.Stream;
+
+/**
+ * Created by Sam on 2019/6/10.
+ */
+public class Array {
+    public static void main(String[] args) {
+        test1();
+
+        test2();
+    }
+
+    private static void test1() {
+        int[] arr = new int[]{1, 3, 5};
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void test2() {
+        Random random = new Random();
+        int[] arr = new int[5];
+        Stream<Integer> limit = Stream.generate(() -> random.nextInt(100)).limit(10);
+//        limit.forEach(System.out::println);
+        Integer integer = limit.max(Comparator.comparingInt(i -> i)).orElse(0);
+        System.out.println(integer);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
