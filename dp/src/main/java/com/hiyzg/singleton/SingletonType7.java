810e73e18af51a39570e4d66d0329fc537536c11
yangzhenguo <908612388@qq.com>
2019-08-16 17:49:40 +0800

    singleton enum

--- dp/src/main/java/com/hiyzg/singleton/SingletonType7.java
+++ dp/src/main/java/com/hiyzg/singleton/SingletonType7.java
@@ -1,5 +1,8 @@
 package com.hiyzg.singleton;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sam on 2019/8/13.
  */
@@ -7,6 +10,8 @@ public class SingletonType7 {
     public static void main(String[] args) {
         System.out.println(Singleton.INSTANCE.hashCode());
         System.out.println(Singleton.INSTANCE.hashCode());
+
+        new BufferedReader(new InputStreamReader(System.in)).lines().forEach(System.out::println);
     }
 }
 
