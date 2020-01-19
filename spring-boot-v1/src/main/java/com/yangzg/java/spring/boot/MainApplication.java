df9385427b5e5f4ec5ae28f1d5b0d7de45470ca1
yangzhenguo <908612388@qq.com>
2020-01-19 10:41:32 +0800

    spring boot main

--- spring-boot-v1/src/main/java/com/yangzg/java/spring/boot/MainApplication.java
+++ spring-boot-v1/src/main/java/com/yangzg/java/spring/boot/MainApplication.java
@@ -10,6 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class);
+        SpringApplication.run(MainApplication.class, args);
     }
 }
